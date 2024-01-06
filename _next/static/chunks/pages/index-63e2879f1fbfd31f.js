c06cb08b509b2cfe68a5fdbe4974db688b7a70fd
Sheil Naik <sheil@Sheils-Mac-mini.local>
2024-01-06 09:55:30 -0500

    Commit files

---
+++ _next/static/chunks/pages/index-63e2879f1fbfd31f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{8396:function(n,u,_){"use strict";_.r(u);var t=_(95242);_(12697),u.default=t.Z},75557:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(8396)}])}},function(n){n.O(0,[774,228,13,445,937,260,265,570,866,874,714,827,451,360,888,179],function(){return n(n.s=75557)}),_N_E=n.O()}]);
